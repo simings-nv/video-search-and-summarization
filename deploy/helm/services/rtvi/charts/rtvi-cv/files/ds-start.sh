e93fe3878a61566f1c8ac81c2c25f8f3d88d7338
Priyank Srivastava <prisrivastav@nvidia.com>
2026-07-15 12:18:30 +0530

    fix helm copy command to dereference symlinks
    Signed-off-by: Priyank Srivastava <prisrivastav@nvidia.com>

--- deploy/helm/services/rtvi/charts/rtvi-cv/files/ds-start.sh
+++ deploy/helm/services/rtvi/charts/rtvi-cv/files/ds-start.sh
@@ -64,7 +64,7 @@ stage_mounted_configs_if_present() {
 
     if [[ "$has_files" == "true" ]]; then
         mkdir -p "$DS_CONFIG_DIR"
-        cp -rL "${DS_MOUNTED_CONFIGS_DIR}/." "${DS_CONFIG_DIR}/"
+        cp -rL --no-preserve=all "${DS_MOUNTED_CONFIGS_DIR}/." "${DS_CONFIG_DIR}/"
         echo "##### Staged profile configs from ${DS_MOUNTED_CONFIGS_DIR} -> ${DS_CONFIG_DIR} #####"
     fi
 }
